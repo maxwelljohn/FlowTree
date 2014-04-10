c6b48429e748ae1a9d2739c22522412b7ebbb855
John Maxwell <dreamalgebra@gmail.com>
2014-04-10 13:06:50 -0700

    Track and output all views that have been opened

--- FlowTree.py
+++ FlowTree.py
@@ -5,9 +5,12 @@ class ExampleCommand(sublime_plugin.TextCommand):
 		self.view.insert(edit, 0, "Hello, World!")
 
 class Logger(sublime_plugin.EventListener):
+	def __init__(self):
+		self.storage = []
 	def on_new(self, view):
 		try:
 			edit = view.begin_edit('test')
-			view.insert(edit, 0, "I spy with my little eye...")
+			self.storage.append(repr(view))
+			view.insert(edit, 0, repr(self.storage))
 		finally:
 			view.end_edit()
