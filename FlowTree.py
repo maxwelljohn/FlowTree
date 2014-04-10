7121adbf24ba65ace1c18902cd0c1c75c60e1b5f
John Maxwell <dreamalgebra@gmail.com>
2014-04-10 15:18:57 -0700

    Basic flow tree implementation

--- FlowTree.py
+++ FlowTree.py
@@ -1,21 +1,37 @@
 import sublime, sublime_plugin
+from collections import namedtuple
+
+ViewNode = namedtuple('ViewNode', ['description', 'children'])
 
 class ECommand(sublime_plugin.TextCommand):
-	msg = "Hello, World!"
+	root_node = ViewNode('root node', [])
+	current_node = root_node
+	node_index = {}
+	@classmethod
+	def record_on_activated(cls, view):
+		vid = view.id()
+		if view.file_name():
+			if vid in cls.node_index:
+				cls.current_node = cls.node_index[vid]
+			else:
+				new_node = ViewNode(view.file_name(), [])
+				cls.current_node.children.append(new_node)
+				cls.node_index[vid] = new_node
+				cls.current_node = new_node
+			print(cls.flow_tree())
+	@classmethod
+	def flow_tree(cls):
+		def show_node(node, indent):
+			result = '  ' * indent
+			result += node.description
+			result += '\n'
+			for child in node.children:
+				result += show_node(child, indent + 1)
+			return result
+		return show_node(cls.root_node, 0)
 	def run(self, edit):
-		self.view.insert(edit, 0, ECommand.msg)
+		self.view.insert(edit, 0, ECommand.flow_tree())
 
 class Logger(sublime_plugin.EventListener):
-	def __init__(self):
-		self.storage = []
-	def log_info(self, view, action):
-		entry = "%s view #%d, window %s, buffer %d, filename %s" % (action, view.id(), repr(view.window()), view.buffer_id(), view.file_name())
-		print(entry)
-		self.storage.append(entry)
-		ECommand.msg = repr(self.storage)
-	def on_new(self, view):
-		self.log_info(view, 'new')
-	def on_load(self, view):
-		self.log_info(view, 'load')
 	def on_activated(self, view):
-		self.log_info(view, 'activated')
+		ECommand.record_on_activated(view)
