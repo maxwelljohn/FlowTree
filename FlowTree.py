19b04cf70c9a026124e6ed20cf7b7bee10771533
John Maxwell <dreamalgebra@gmail.com>
2014-04-10 14:30:42 -0700

    Improve logging to better understand Sublime

--- FlowTree.py
+++ FlowTree.py
@@ -1,17 +1,21 @@
 import sublime, sublime_plugin
 
-class ExampleCommand(sublime_plugin.TextCommand):
+class ECommand(sublime_plugin.TextCommand):
 	msg = "Hello, World!"
 	def run(self, edit):
-		self.view.insert(edit, 0, ExampleCommand.msg)
+		self.view.insert(edit, 0, ECommand.msg)
 
 class Logger(sublime_plugin.EventListener):
 	def __init__(self):
 		self.storage = []
+	def log_info(self, view, action):
+		entry = "%s view #%d, window %s, buffer %d, filename %s" % (action, view.id(), repr(view.window()), view.buffer_id(), view.file_name())
+		print(entry)
+		self.storage.append(entry)
+		ECommand.msg = repr(self.storage)
 	def on_new(self, view):
-		try:
-			edit = view.begin_edit('test')
-			self.storage.append(repr(view))
-			ExampleCommand.msg = repr(self.storage)
-		finally:
-			view.end_edit(edit)
+		self.log_info(view, 'new')
+	def on_load(self, view):
+		self.log_info(view, 'load')
+	def on_activated(self, view):
+		self.log_info(view, 'activated')
