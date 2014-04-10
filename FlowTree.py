97533974e04ac4ed9d2b8594e8e2e2f802655474
John Maxwell <dreamalgebra@gmail.com>
2014-04-10 13:03:40 -0700

    Add text to new Sublime views

--- FlowTree.py
+++ FlowTree.py
@@ -3,3 +3,11 @@ import sublime, sublime_plugin
 class ExampleCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		self.view.insert(edit, 0, "Hello, World!")
+
+class Logger(sublime_plugin.EventListener):
+	def on_new(self, view):
+		try:
+			edit = view.begin_edit('test')
+			view.insert(edit, 0, "I spy with my little eye...")
+		finally:
+			view.end_edit()
