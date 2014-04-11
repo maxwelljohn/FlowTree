bb471898343d0a617c6e978a3ca30d84627317d3
John Maxwell <dreamalgebra@gmail.com>
2014-04-10 18:08:23 -0700

    Add menu command to display FlowTree

--- FlowTree.py
+++ FlowTree.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 
 ViewNode = namedtuple('ViewNode', ['description', 'children'])
 
-class ECommand(sublime_plugin.TextCommand):
+class ECommand(sublime_plugin.WindowCommand):
 	root_node = ViewNode('root node', [])
 	current_node = root_node
 	node_index = {}
@@ -18,7 +18,7 @@ class ECommand(sublime_plugin.TextCommand):
 				cls.current_node.children.append(new_node)
 				cls.node_index[vid] = new_node
 				cls.current_node = new_node
-			print(cls.flow_tree())
+				print(cls.flow_tree())
 	@classmethod
 	def flow_tree(cls):
 		def show_node(node, indent):
@@ -29,8 +29,14 @@ class ECommand(sublime_plugin.TextCommand):
 				result += show_node(child, indent + 1)
 			return result
 		return show_node(cls.root_node, 0)
-	def run(self, edit):
-		self.view.insert(edit, 0, ECommand.flow_tree())
+	def run(self):
+		view = self.window.new_file()
+		view.set_name('Your FlowTree')
+		edit = view.begin_edit('DisplayFlowTree')
+		try:
+			view.insert(edit, 0, ECommand.flow_tree())
+		finally:
+			view.end_edit(edit)
 
 class Logger(sublime_plugin.EventListener):
 	def on_activated(self, view):
